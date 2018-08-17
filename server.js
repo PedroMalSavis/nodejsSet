f53a8de461d3bab4b037714f667b179d844ef71f
Pedro Juan <petertitoy@protonmail.com>
2018-08-17 21:36:39 +0800

    added express-vue and use it as new boilerplate

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-// TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
-
-var jsonData = {count: 12, message: 'hey'};
-var data = [
-  {data:1, name:'pedro'},
-  {data:2, name:'juan'},
-  {data:3, name:'kennedy'}
-]
+const expressVue = require('express-vue');
 const express = require('express');
+
 const app = express();
 
-
-app.get('/', function(res, req){
-  res.json(data);
+const port = 3000;
+app.listen(port, function(){
+  console.log(`app is running on localhost:${port}`);
 })
-app.listen(3000);
