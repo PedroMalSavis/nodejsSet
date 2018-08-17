ad34141ca584ba9b73c03e7682940fef4843df05
Pedro Juan <petertitoy@protonmail.com>
2018-08-17 16:59:31 +0800

    added basic express config

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+var data = [
+  {data:1, name:'pedro'},
+  {data:2, name:'juan'},
+  {data:3, name:'kennedy'}
+]
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(res, req){
+  res.json(data);
+})
+app.listen(3000);
